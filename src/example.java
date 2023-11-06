cf17e71ac9774d7e553ad15bd06f0040fc876b6b
deasunara <deasunara@gmail.com>
2023-11-07 03:31:02 +0700

    commit kedua

---
+++ src/example.java
@@ -0,0 +1,9 @@
+public class example {
+    public static  void test (){
+        System.out.println("hallo");
+        System.out.println("selamat pagi");
+    }
+    public static void main(String[] args) {
+        test();
+    }
+}
